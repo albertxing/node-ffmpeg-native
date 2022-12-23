cd56636fbb249298468274fb257d1102c1aaa681
Albert Xing <albertxing@users.noreply.github.com>
2022-12-23 15:18:03 -0800

    Update build.sh

--- src/win/build.sh
+++ src/win/build.sh
@@ -3,3 +3,5 @@ cd ffmpeg
 export PATH="/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/":$PATH
 ./configure --target-os=win64 --arch=x86_64 --toolchain=msvc
 make -j4
+cd ..
+make libffmpeg.a
