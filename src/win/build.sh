f8f73687c2326fd37aa77a9c3781cc4a27c25122
Albert Xing <albertxing@users.noreply.github.com>
2022-12-23 20:21:33 -0800

    add nvenc headers

--- src/win/build.sh
+++ src/win/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -x
+
+git clone https://github.com/FFmpeg/nv-codec-headers
+cd nv-codec-headers
+make install PREFIX=/usr
+cd ..
+
 cd ffmpeg
 export PATH="/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/":$PATH
 ./configure --target-os=win64 --arch=x86_64 --toolchain=msvc
-make -j4
+echo make -j4
 cd ..
 make libffmpeg.a
