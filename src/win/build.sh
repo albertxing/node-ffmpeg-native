33db2b4998057f5a43cccb51d496abfce1eb858d
Albert Xing <albertxing@users.noreply.github.com>
2022-12-24 17:09:41 -0800

    Update build.sh

--- src/win/build.sh
+++ src/win/build.sh
@@ -6,9 +6,12 @@ cd nv-codec-headers
 make install PREFIX=/usr
 cd ..
 
+git clone https://github.com/GPUOpen-LibrariesAndSDKs/AMF
+cp -r AMF/amf/public/include /usr/include/AMF
+
 cd ffmpeg
 export PATH="/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/":$PATH
-./configure --target-os=win64 --arch=x86_64 --toolchain=msvc
+./configure --target-os=win64 --arch=x86_64 --toolchain=msvc --extra-cflags="-DCommandLineToArgvW=_CommandLineToArgvW_"
 make -j4
 cd ..
 make libffmpeg.a
